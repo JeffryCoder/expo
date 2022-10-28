b78c2525689cd5be587276a0fe447ab3b7bb4545
jeffrycoder <jeffrycoder@gmail.com>
2022-10-28 13:56:02 -0400

    Funcionalidad de cargar imagen agregada

--- App.js
+++ App.js
@@ -1,23 +1,48 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import logo from './assets/logo.png';	
 import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = React.useState(null);
+
   let openImagePickerAsync = async () => {
     let pickerResult = await ImagePicker.launchImageLibraryAsync();
-    console.log(pickerResult);
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+    setSelectedImage({ localUri: pickerResult.uri });
+
+    console.log(pickerResult); // for debugging
+  };
+
+  if (selectedImage !== null) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{ uri: selectedImage.localUri }}
+          style={styles.thumbnail}
+        />
+      </View>
+  
+    );
   };
   return (
     <View style={styles.container}>
+
       <Image source={{ uri: "https://cdn.pixabay.com/photo/2016/11/25/23/15/moon-1859616_960_720.jpg"}} style={styles.logo} />
       <Text style={styles.instructions}>¡Para compartir una foto desde tu teléfono con un amigo, solo presiona el botón de abajo!</Text>
-      <Text style={styles.instructions}>QUE DUROOOOO NELSOOOOOOOOOOOOOOOOOON</Text>
-      <Text style={styles.instructions}>Klk Esmarlyn</Text>
+      <Text style={styles.instructions}></Text>
+      
       <StatusBar style="auto" />
+  
+      
       <TouchableOpacity onPress={openImagePickerAsync} style={styles.button}>
         <Text style={styles.buttonText}>Selecciona una foto</Text>
-        </TouchableOpacity>
+      </TouchableOpacity>
+
+      <Text style={styles.instructions}>Por JeffryCoder</Text>
     </View>
   );
 }
@@ -48,6 +73,11 @@ const styles = StyleSheet.create({
   buttonText: {
     fontSize: 20,
     color: '#fff',
+  },
+  thumbnail: {
+    width: 300,
+    height: 300,
+    resizeMode: "stretch",
   },
 
 });
